e0a1932f8c3a7cdec21a69218ab1bf425db5bff9
ZealGiant-Tech <68040281+ZealGiant-Tech@users.noreply.github.com>
2020-07-09 10:48:41 +0800

    Add files via upload

---
+++ build/CMakeFiles/capture.dir/DependInfo.cmake
@@ -0,0 +1,89 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zealgiant/MaskCheck/main.cpp" "/home/zealgiant/MaskCheck/build/CMakeFiles/capture.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "OPENCV_TRAITS_ENABLE_DEPRECATED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/eigen3"
+  "/home/zealgiant/ZGT/opencv-3.4.3/build"
+  "/home/zealgiant/ZGT/opencv-3.4.3/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/include/opencv"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudev/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/core/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudaarithm/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/flann/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/imgproc/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/ml/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/objdetect/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/phase_unwrapping/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/plot/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/reg/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/surface_matching/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/video/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/xphoto/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudabgsegm/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudafilters/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudaimgproc/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudawarping/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/dnn/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/freetype/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/fuzzy/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/hfs/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/imgcodecs/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/photo/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/shape/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/videoio/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/xobjdetect/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudacodec/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/face/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/highgui/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/bioinspired/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/dnn_objdetect/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/dpm/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/features2d/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/line_descriptor/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/saliency/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/text/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/calib3d/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/ccalib/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudafeatures2d/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudalegacy/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudaobjdetect/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudaoptflow/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/cudastereo/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/datasets/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/rgbd/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/stereo/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/structured_light/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/superres/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/tracking/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/videostab/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/xfeatures2d/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/ximgproc/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/aruco/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/bgsegm/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/optflow/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/stitching/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/world/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/opencv_contrib-3.4.3/modules/img_hash/include"
+  "/home/zealgiant/ZGT/opencv-3.4.3/modules/ts/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
